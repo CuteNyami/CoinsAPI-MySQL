ea622b4d0df082d18b020a4fe5125fe2cada180b
Bylumia <000Legomaster000@gmail.com>
2021-12-16 17:20:15 +0100

    CoinsAPI

--- CoinsAPI-master/src/main/java/me/cutenyami/coinsapi/plugin/utils/Utils.java
+++
@@ -1,8 +0,0 @@
-package me.cutenyami.coinsapi.plugin.utils;
-
-public class Utils {
-
-    public static final String PREFIX = "§9» §7";
-    public static final String ERROR_PREFIX = "§c» §7";
-
-}
